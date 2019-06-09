d40945355e165de89c538c6011bef2ebffc80a40
evanweible-wf <evan.weible@workiva.com>
2019-06-09 16:02:16 -0500

    Setup dockerfile build.

--- test/util.dart
+++ test/util.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:test/test.dart';
@@ -10,9 +11,14 @@ void activateWebdev(String constraint) {
   expect(procResult.exitCode, 0,
       reason: 'Failed to global activate webdev $constraint:\n'
           '${procResult.stdout}');
+  expect(isWebdevGlobalActivated(), isTrue,
+      reason: "Webdev should be globally active, but isn't.");
 }
 
 void deactivateWebdev() {
+  if (!isWebdevGlobalActivated()) {
+    return;
+  }
   final procResult = Process.runSync(
     'pub',
     ['global', 'deactivate', 'webdev'],
@@ -20,4 +26,15 @@ void deactivateWebdev() {
   expect(procResult.exitCode, 0,
       reason: 'Failed to global deactivate webdev:\n'
           '${procResult.stdout}');
+  expect(isWebdevGlobalActivated(), isFalse,
+      reason: 'Webdev should not be globally active, but is.');
+}
+
+bool isWebdevGlobalActivated() {
+  final procResult = Process.runSync(
+    'pub',
+    ['global', 'list'],
+    stdoutEncoding: utf8,
+  );
+  return procResult.stdout.toString().contains('webdev');
 }
